386520d975759fb72d1f8258fa700b17d93961db
sgchoi <csg5402@naver.com>
2026-06-26 11:48:32 +0900

    webtm 0.6.0: clean formula structure (install at top level, robust test)

--- Formula/webtm.rb
+++ Formula/webtm.rb
@@ -13,18 +13,10 @@ class Webtm < Formula
     on_intel do
       url "https://github.com/sgchoi/webtm-dist/releases/download/v0.6.0/webtm_0.6.0_darwin_amd64.tar.gz"
       sha256 "20b55b78b4d19fa19b8a8c94cf6e46a39e9fef27630033daa6effbc07d77ac50"
-
-      def install
-        bin.install "webtm"
-      end
     end
     on_arm do
       url "https://github.com/sgchoi/webtm-dist/releases/download/v0.6.0/webtm_0.6.0_darwin_arm64.tar.gz"
       sha256 "c2b524063b5fbab64eeb744b08ebe04ca1db9fd04b6f392b6f40a7b785030353"
-
-      def install
-        bin.install "webtm"
-      end
     end
   end
 
@@ -32,22 +24,18 @@ class Webtm < Formula
     on_intel do
       url "https://github.com/sgchoi/webtm-dist/releases/download/v0.6.0/webtm_0.6.0_linux_amd64.tar.gz"
       sha256 "53027717625a1566f08bdda62c3214c5a8abd6d763a31ad6314ee0852b435cca"
-
-      def install
-        bin.install "webtm"
-      end
     end
     on_arm do
       url "https://github.com/sgchoi/webtm-dist/releases/download/v0.6.0/webtm_0.6.0_linux_arm64.tar.gz"
       sha256 "40401070c665103a11bcc4098f39b4e30100d348bb33f43b35752f325db9c85f"
-
-      def install
-        bin.install "webtm"
-      end
     end
   end
 
+  def install
+    bin.install "webtm"
+  end
+
   test do
-    system "#{bin}/webtm", "version"
+    assert_match "webtm version", shell_output("#{bin}/webtm version")
   end
 end
