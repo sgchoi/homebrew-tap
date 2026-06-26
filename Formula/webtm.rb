f86f2a67eaaa9c349dd3f31263a6d63c1b9d8dae
sgchoi <csg5402@naver.com>
2026-06-26 09:37:41 +0900

    webtm 0.6.0 (manual backfill; auto-generated henceforth)

---
+++ Formula/webtm.rb
@@ -0,0 +1,52 @@
+# typed: false
+# frozen_string_literal: true
+
+# This file was generated to match GoReleaser output; the next release overwrites it.
+class Webtm < Formula
+  desc "WebTerm - browser-based terminal access tool"
+  homepage "https://github.com/sgchoi/webterminal"
+  version "0.6.0"
+  license "MIT"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/sgchoi/webterminal/releases/download/v0.6.0/webtm_0.6.0_darwin_amd64.tar.gz"
+      sha256 "20b55b78b4d19fa19b8a8c94cf6e46a39e9fef27630033daa6effbc07d77ac50"
+
+      def install
+        bin.install "webtm"
+      end
+    end
+    on_arm do
+      url "https://github.com/sgchoi/webterminal/releases/download/v0.6.0/webtm_0.6.0_darwin_arm64.tar.gz"
+      sha256 "c2b524063b5fbab64eeb744b08ebe04ca1db9fd04b6f392b6f40a7b785030353"
+
+      def install
+        bin.install "webtm"
+      end
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/sgchoi/webterminal/releases/download/v0.6.0/webtm_0.6.0_linux_amd64.tar.gz"
+      sha256 "53027717625a1566f08bdda62c3214c5a8abd6d763a31ad6314ee0852b435cca"
+
+      def install
+        bin.install "webtm"
+      end
+    end
+    on_arm do
+      url "https://github.com/sgchoi/webterminal/releases/download/v0.6.0/webtm_0.6.0_linux_arm64.tar.gz"
+      sha256 "40401070c665103a11bcc4098f39b4e30100d348bb33f43b35752f325db9c85f"
+
+      def install
+        bin.install "webtm"
+      end
+    end
+  end
+
+  test do
+    system "#{bin}/webtm", "--version"
+  end
+end
